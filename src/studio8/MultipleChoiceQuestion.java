d1af5e0da053fdaf78bbcde00181fffc9497c164
saranyaganne <saranyaganne@localhost>
2025-04-02 15:31:19 -0500

    question and mc is done

--- src/studio8/MultipleChoiceQuestion.java
+++ src/studio8/MultipleChoiceQuestion.java
@@ -1,41 +1,35 @@
-package studio8;
-
-import support.cse131.NotYetImplementedException;
-
-public class MultipleChoiceQuestion extends Question {
+class MultipleChoiceQuestion extends Question {
+	private String[] choices;
 
 	/**
 	 * Constructor
-	 * @param prompt
-	 * @param answer
-	 * @param points
-	 * @param choices
+	 * @param prompt the question prompt
+	 * @param answer the correct answer
+	 * @param points the number of points the question is worth
+	 * @param choices the possible answer choices
 	 */
 	public MultipleChoiceQuestion(String prompt, String answer, int points, String[] choices) {
-		// Call the super class constructor, then create and set
-		// instance variables for any values that aren't handled
-		// by the base class
-		throw new NotYetImplementedException();
+		super(prompt, answer, points);
+		this.choices = choices;
 	}
-	
+
 	/**
 	 * Display the prompt for the question in addition to 
 	 * the choices present for the question.
 	 */
+	@Override
 	public void displayPrompt() {
-		throw new NotYetImplementedException();
+		super.displayPrompt();
+		for (int i = 0; i < choices.length; i++) {
+			System.out.println((i + 1) + ". " + choices[i]);
+		}
 	}
-	
+
 	/**
 	 * Getter method for the available choices
 	 * @return String[] of choices
 	 */
 	public String[] getChoices() {
-		throw new NotYetImplementedException();
+		return this.choices;
 	}
-	
-	public static void main(String[] args) {
-		// TODO: create your own MultipleChoiceQuestion
-	}
-
-}
+} 
--- src/studio8/Question.java
+++ src/studio8/Question.java
@@ -3,55 +3,56 @@ package studio8;
 import support.cse131.NotYetImplementedException;
 
 public class Question {
-	
+	private String prompt;
+	private String answer;
+	private int points;
+
 	/**
 	 * Constructor
-	 * @param prompt
-	 * @param answer
-	 * @param points
+	 * @param prompt the question prompt
+	 * @param answer the correct answer
+	 * @param points the number of points the question is worth
 	 */
 	public Question(String prompt, String answer, int points) {
-		throw new NotYetImplementedException();
+		this.prompt = prompt;
+		this.answer = answer;
+		this.points = points;
 	}
-	
+
 	/**
 	 * Prints out the current question's prompt, with a parenthetical 
 	 * number of points possible.
 	 */
 	public void displayPrompt() {
-		System.out.println(this.prompt + "(" + this.points + " points)");
+		System.out.println(this.prompt + " (" + this.points + " points)");
 	}
-	
+
 	/**
 	 * Check the answer provided by a user
-	 * @param givenAnswer
+	 * @param givenAnswer the user's submitted answer
 	 * @return the number of points earned by the givenAnswer
 	 */
 	public int checkAnswer(String givenAnswer) {
-		if(answer.equals(givenAnswer)) { //String comparison
+		if (answer.equals(givenAnswer)) {
 			return this.points;
 		} else {
 			return 0;
 		}
 	}
-	
+
 	/**
 	 * Getter method for the points possible
 	 * @return int points
 	 */
 	public int getPoints() {
-		throw new NotYetImplementedException();
+		return this.points;
 	}
-	
+
 	/**
 	 * Getter method for the answer to the question
 	 * @return String answer
 	 */
 	public String getAnswer() {
-		throw new NotYetImplementedException();
-	}
-	
-	public static void main(String[] args) {
-		// TODO: Create a Question object of your own!
+		return this.answer;
 	}
 }
